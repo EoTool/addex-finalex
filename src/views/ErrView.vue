e90321aa99eb61ceced18f7f47ce8cb634714f1b
Shawnnectar <shawnmctc@gmail.com>
2023-10-15 17:21:14 -0300

    [M]

--- src/views/ErrView.vue
+++ src/views/ErrView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import {ref, watch} from 'vue';
+import {useRoute} from 'vue-router';
 
 const route = useRoute();
 const currentPage = ref('');
@@ -24,21 +24,21 @@ setCurrentPage();
 
 
 <template>
-  <div class="flex flex-col items-center pt-56">
+  <div class="flex flex-col items-center pt-10 amm:pt-56">
     <h1 class="font-PhonkRegular text-5xl mmd:text-6xl mx-6" style="color: #017DF9">Que pena</h1>
-    <text class="hidden-visible font-NexaHeavy text-lg mx-6 pt-3">A página de <span class="text-red-600">{{ currentPage }}</span> que você procura ainda está em desenvolvimento.</text>
-    <text class="font-NexaLight text-base pt-4 md:text-center md:pt-0">
-      Pedimos desculpas pela inconveniência. Estamos trabalhando duro para criar <br class="mmd:hidden" /> um espaço incrível para você em breve. Fique atento às atualizações e volte em breve para conferir!
+    <text class="hidden-visible text-white font-NexaHeavy text-lg mx-6 pt-20 amm:pt-3 amm:text-center">A página de <span class="text-red-600">{{
+        currentPage
+      }}</span> que você procura ainda está em desenvolvimento.
     </text>
-
+    <text class="font-NexaBook text-white text-base mx-6 pt-8 amm:pt-4 amm:text-center">
+      Pedimos desculpas pela inconveniência. Estamos trabalhando duro para criar um espaço incrível para você em breve.
+    </text>
+    <text class="font-NexaBook text-blue-200 text-base mx-6 pt-20 amm:pt-2 text-center ">Fique atento às atualizações e volte em breve para conferir<span class="">!</span></text>
 
   </div>
 </template>
 
 <style scoped>
-text {
-  color: white;
-}
 
 @media (min-width: 850px) {
   .hidden-visible {
