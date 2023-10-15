f4de46d27fe31210f02e3d563d6ee470aec2450c
Shawnnectar <shawnmctc@gmail.com>
2023-10-15 17:05:11 -0300

    [M]

--- src/views/ErrView.vue
+++ src/views/ErrView.vue
@@ -25,15 +25,24 @@ setCurrentPage();
 
 <template>
   <div class="flex flex-col items-center pt-56">
-    <h1 class="font-PhonkRegular text-6xl" style="color: #017DF9">Que pena</h1>
-    <text class="font-NexaHeavy text-lg pt-2">A página <span class="">{{ currentPage }}</span> que você deseja acessar ainda não foi construída.</text>
-    <text class="font-NexaLight text-base pt-5" style="line-height: 90%">Pedimos desculpas pela inconveniência. Estamos trabalhando duro para criar</text>
-    <text class="font-NexaLight text-base">um espaço incrível para você em breve. Fique atento às atualizações e volte em breve para conferir!</text>
+    <h1 class="font-PhonkRegular text-5xl mmd:text-6xl mx-6" style="color: #017DF9">Que pena</h1>
+    <text class="hidden-visible font-NexaHeavy text-lg mx-6 pt-3">A página de <span class="text-red-600">{{ currentPage }}</span> que você procura ainda está em desenvolvimento.</text>
+    <text class="font-NexaLight text-base pt-4 md:text-center md:pt-0">
+      Pedimos desculpas pela inconveniência. Estamos trabalhando duro para criar <br class="mmd:hidden" /> um espaço incrível para você em breve. Fique atento às atualizações e volte em breve para conferir!
+    </text>
+
+
   </div>
 </template>
 
 <style scoped>
 text {
   color: white;
+}
+
+@media (min-width: 850px) {
+  .hidden-visible {
+    display: block;
+  }
 }
 </style>
