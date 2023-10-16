35a18c1f26798745e1de96b94024bbd0b27a959e
Shawnnectar <shawnmctc@gmail.com>
2023-10-15 21:18:08 -0300

    [M]

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -12,17 +12,25 @@ export default {
 <template>
   <div class="flex flex-col mmd:flex-row mmd:justify-between height-difuser">
 
-    <div class="flex flex-col <-> xl:pl-48 lg:pl-24 md:pl-20 sm:pl-24 pl-10 <-> xl:py-52 lg:py-48 md:py-16 sm:py-14 py-6">
+    <div
+        class="flex flex-col <-> xl:pl-48 lg:pl-24 md:pl-20 sm:pl-24 pl-10 <-> xl:py-52 lg:py-48 md:py-16 sm:py-14 py-6">
 
-      <text class="text-4xl amm:text-5xl axm:text-6xl sm:text-7xl font-PhonkRegular " style="line-height: 80%; color: #017DF9">Addex</text>
-      <text class="text-4xl amm:text-5xl axm:text-6xl sm:text-7xl text-white font-PhonkRegular" style="line-height: 90%">Creations</text>
-      <text class="text-xs amm:text-xl sm:text-2xl text-white font-NexaBook sm:leading-10">Transformando sua visão em arte.</text>
+      <text class="text-4xl amm:text-5xl axm:text-6xl sm:text-7xl font-PhonkRegular "
+            style="line-height: 80%; color: #017DF9">Addex
+      </text>
+      <text class="text-4xl amm:text-5xl axm:text-6xl sm:text-7xl text-white font-PhonkRegular"
+            style="line-height: 90%">Creations
+      </text>
+      <text class="text-xs amm:text-xl sm:text-2xl text-white font-NexaBook sm:leading-10">Transformando sua visão em
+        arte.
+      </text>
 
       <button class="saiba-mais mt-14" @click="redirectToDiscord">Saiba mais +</button>
 
     </div>
 
-    <div class="flex mmd:flex-col flex-row mmd:justify-self-end xl:px-48 lg:px-24 md:px-20 mmd:px-20 sm:px-28 px-10 <-> xl:pt-32 lg:pt-32">
+    <div
+        class="flex mmd:flex-col flex-row mmd:justify-self-end xl:px-48 lg:px-24 md:px-20 mmd:px-20 sm:px-28 px-10 <-> xl:pt-32 lg:pt-32">
 
       <div class="flex flex-col text-right pr-4 py-7">
         <text class="font-NexaHeavy text-4xl" style="color: #017DF9">+<span class="text-white">200</span></text>
@@ -33,7 +41,8 @@ export default {
       <div class="flex flex-col text-right px-4 py-7">
         <text class="font-NexaHeavy text-4xl" style="color: #017DF9">+<span class="text-white">4</span></text>
         <text class="font-NexaLight text-white">Anos no</text>
-        <text class="font-NexaLight text-white">mercado</text></div>
+        <text class="font-NexaLight text-white">mercado</text>
+      </div>
 
       <div class="flex flex-col text-right px-4 py-7">
         <text class="font-NexaHeavy text-4xl" style="color: #017DF9">+<span class="text-white">380</span></text>
@@ -46,15 +55,31 @@ export default {
   </div>
   <div class="pb-12">
     <footer class="py-0 xl:mx-32 lg:mx-24 md:mx-20 mx-10 dark:bg-zinc-950 dark:text-gray-50 rounded-[48px]">
-      <div class="container py-10 mx-auto px-10 space-y-6 divide-y-2 divide-neutral-900 md:space-y-12 divide-opacity-50">
-        <div class="grid grid-cols-12">
+      <div
+          class="container py-10 mx-auto px-10 space-y-6 divide-y-2 divide-neutral-900 md:space-y-12 divide-opacity-50">
+        <div class="grid grid-cols-12 items-center justify-between w-fit">
           <div class="pb-0 -mb-4">
-              <div class="flex items-center justify-center w-12 h-12">
-                <img src="@/assets/addex_blue_logo.svg" alt="Addex Gray" class="dark:text-gray-900">
-              </div>
+            <div class="flex items-center justify-center w-12 h-12">
+              <img src="@/assets/addex_blue_logo.svg" alt="Addex Gray" class="dark:text-gray-900">
+            </div>
           </div>
+          <div class="appender pb-0 -mb-4 pl-4 col-end-9 amm:col-end-10 sm:col-end-11 lg:col-end-12">
+            <div class="flex items-center justify-center">
+              <a  href="https://www.behance.net/addex" target="_blank">
+                <img class="mx-4 pr-4 amm:pr-6 sm:pr-10 xl:pr-16" src="@/assets/behance_gray_logo.png" alt="Behance">
+              </a>
+              <a href="https://discord.gg/K9epKN7qbG" target="_blank">
+                <img class="mx-4 pr-4 amm:pr-6 sm:pr-10 xl:pr-16" src="@/assets/discord_gray_logo.png" alt="Discord">
+              </a>
+              <a href="https://www.instagram.com/addexcompany/" target="_blank">
+                <img class="mx-4 pr-4 amm:pr-6 sm:pr-10 xl:pr-16" src="@/assets/instagram_gray_logo.png" alt="Instagram">
+              </a>
+            </div>
+          </div>
+
         </div>
-        <div class="grid justify-center pt-11 lg:justify-between">
+
+        <div class="grid justify-center pt-8 lg:justify-between">
           <div class="flex flex-col self-center text-sm text-center lg:mr-10 mb-4 md:block space-x-6">
     <span class="font-NexaBook text-base text-neutral-400">
       Copyright © 2019 - 2023 Addex Company.
@@ -92,6 +117,12 @@ export default {
 @media (min-width: 768px) {
   .height-difuser {
     min-height: calc(100vh - 116px);
+  }
+}
+
+@media (min-width: 1460px) {
+  .appender {
+
   }
 }
 
